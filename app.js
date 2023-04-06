8d333adb6034faf50f6723c0b2eb26066dd732ec
My Duyen <duyenb1910046@student.ctu.edu.vn>
2023-04-06 09:22:09 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const contacrsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -13,5 +14,15 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/contacts", contacrsRouter);
+
+//handle 404 response
+app.use((req, res, next) => {
+    //Middleware xử lý lỗi tập trung
+    //Trong các đoạn code xử lý ở các route, gọi next(error)
+    //  sẽ chuyển về middleware xử lý lỗi này
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;
